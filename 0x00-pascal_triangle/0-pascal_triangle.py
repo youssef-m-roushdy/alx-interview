dbda27e790cda86443e54fb756440792f87062b3
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2024-07-25 18:49:02 +0300

    .

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -4,7 +4,6 @@
 """
 
 
-
 def pascal_triangle(n):
     """
     Returns a list of integers
@@ -14,13 +13,13 @@ def pascal_triangle(n):
     if n <= 0:
         return []
 
-    triangle = [[1]]  
+    triangle = [[1]]
 
     for i in range(1, n):
-        row = [1]  
+        row = [1]
         for j in range(1, i):
             row.append(triangle[i-1][j-1] + triangle[i-1][j])
-        row.append(1) 
+        row.append(1)
         triangle.append(row)
 
     return triangle
