d4ec0f317f9fbfd3a981c485e42ae855d57d394e
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2024-09-03 23:55:04 +0300

    .

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,45 +1,32 @@
 #!/usr/bin/node
+
 const request = require('request');
-const args = process.argv.slice(2);
-
-if (args.length !== 1) {
-  process.exit(1);
-}
-
-const filmPart = Number.parseInt(args[0]);
-
-const url = `https://swapi-api.alx-tools.com/api/films/${filmPart}`;
 
 const getFilmCharacterName = (characterApi) => {
   request.get(characterApi, (error, response, body) => {
     if (error) {
       console.error('Error occurred:', error);
       return;
-    }
-    if (response.statusCode === 200) {
+    } else {
       const data = JSON.parse(body);
       console.log(data.name);
-    } else {
-      console.error('Failed to retrieve data. Status code:', response.statusCode);
     }
   });
 };
 
 const getFilmApiCharacters = () => {
-  request.get(url, (error, response, body) => {
-    if (error) {
-      console.error('Error occurred:', error);
-      return;
-    }
-    if (response.statusCode === 200) {
-      const data = JSON.parse(body);
-      data.characters.forEach((character) => {
-        getFilmCharacterName(character);
-      });
-    } else {
-      console.error('Failed to retrieve data. Status code:', response.statusCode);
-    }
-  });
+  request.get(`https://swapi-api.hbtn.io/api/films/${process.argv[2]}`,
+    (error, response, body) => {
+      if (error) {
+        console.error('Error occurred:', error);
+        return;
+      } else {
+        const data = JSON.parse(body);
+        data.characters.forEach((character) => {
+          getFilmCharacterName(character);
+        });
+      }
+    });
 };
 
 getFilmApiCharacters();
