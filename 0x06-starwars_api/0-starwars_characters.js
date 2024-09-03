d46bb05a81a57d7ed5dff81cadcbe44381476dda
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2024-09-03 23:50:34 +0300

    .

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-
+const request = require('request');
 const args = process.argv.slice(2);
 
 if (args.length !== 1) {
@@ -10,29 +10,36 @@ const filmPart = Number.parseInt(args[0]);
 
 const url = `https://swapi-api.alx-tools.com/api/films/${filmPart}`;
 
-const createRequest = (url) => {
-  const request = new Request(url, {
-    method: 'GET',
-    headers: new Headers({
-      'Content-Type': 'application/json'
-    })
-  });
-  return request;
-};
-
-const getFilmApiCharacters = async () => {
-  const response = await fetch(createRequest(url));
-  const data = await response.json();
-  return data.characters;
-};
-
-const getFilmCharacters = async () => {
-  const charactersApi = await getFilmApiCharacters();
-  charactersApi.forEach(async (characterApi) => {
-    const response = await fetch(createRequest(characterApi));
-    const data = await response.json();
-    console.log(data.name);
+const getFilmCharacterName = (characterApi) => {
+  request.get(characterApi, (error, response, body) => {
+    if (error) {
+      console.error('Error occurred:', error);
+      return;
+    }
+    if (response.statusCode === 200) {
+      const data = JSON.parse(body);
+      console.log(data.name);
+    } else {
+      console.error('Failed to retrieve data. Status code:', response.statusCode);
+    }
   });
 };
 
-getFilmCharacters();
+const getFilmApiCharacters = () => {
+  request.get(url, (error, response, body) => {
+    if (error) {
+      console.error('Error occurred:', error);
+      return;
+    }
+    if (response.statusCode === 200) {
+      const data = JSON.parse(body);
+      data.characters.forEach((character) => {
+        getFilmCharacterName(character);
+      });
+    } else {
+      console.error('Failed to retrieve data. Status code:', response.statusCode);
+    }
+  });
+};
+
+getFilmApiCharacters();
