c01e0806e70dd21f54ad6f9fe5d30e81aa38bca2
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2024-09-25 23:37:05 +0300

    .

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Find perimeter of the island
+"""
+
+
+def island_perimeter(grid):
+    """Method returns the perimeter of the island described in grid.
+    """
+    a = 0
+    for i in range(0, len(grid)):
+        for j in range(0, len(grid[i])):
+            if grid[i][j] == 1:
+                if i+1 >= len(grid[i]) or grid[i+1][j] == 0:
+                    a += 1
+                if j+1 >= len(grid[i]) or grid[i][j+1] == 0:
+                    a += 1
+                if i-1 == -1 or grid[i-1][j] == 0:
+                    a += 1
+                if j-1 == -1 or grid[i][j-1] == 0:
+                    a += 1
+    return a
