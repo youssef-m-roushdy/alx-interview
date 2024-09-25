282ccbcffdb872bab08c3c5409adc8c7ee1bea23
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2024-09-25 23:37:55 +0300

    .

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -10,12 +10,12 @@ def island_perimeter(grid):
     for i in range(0, len(grid)):
         for j in range(0, len(grid[i])):
             if grid[i][j] == 1:
-                if i+1 >= len(grid[i]) or grid[i+1][j] == 0:
+                if i + 1 >= len(grid) or grid[i + 1][j] == 0:
                     a += 1
-                if j+1 >= len(grid[i]) or grid[i][j+1] == 0:
+                if j + 1 >= len(grid[i]) or grid[i][j + 1] == 0:
                     a += 1
-                if i-1 == -1 or grid[i-1][j] == 0:
+                if i - 1 < 0 or grid[i - 1][j] == 0:
                     a += 1
-                if j-1 == -1 or grid[i][j-1] == 0:
+                if j - 1 < 0 or grid[i][j - 1] == 0:
                     a += 1
     return a
